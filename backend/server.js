08ab50bb2d0a5efb2730b6c50272dd7b1ce3bf5d
astminsk <astminsk@gmail.com>
2021-09-11 17:36:33 +0300

    another commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const recipeRoutes = require('./routes/recipeRoutes');
+const path = require('path');
 
 connectDB();
 
@@ -10,7 +11,18 @@ const app = express();
 
 app.use(express.json());
 
+//Use Routes
 app.use('/api/recipes', recipeRoutes)
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
